681d0e6b3641b387c375ea54cfb630d3ff2993b6
Abhijitam01 <abhijitamdubey31@gmail.com>
2026-01-28 00:56:50 +0530

    adding the docker

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "./packages/db/prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+});
